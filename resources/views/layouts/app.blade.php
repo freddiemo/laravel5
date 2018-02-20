48646bfe6a78719e5a604bc810e27c0d3248de62
freddie molina <freddiejmo@gmail.com>
2018-02-20 10:58:58 -0430

    defining the app layout

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,19 @@
+<!-- rerources/views/layouts/app.blade.php -->
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Laravel5.5 Quickstart - Basic</title>
+
+        <!-- CSS And JavaScript --> 
+    </head>
+
+    <body>
+        <div class="container">
+            <nav class="navbar navbar-default">
+                <!-- Navbar Contents -->
+            </nav>
+        </div>
+
+        @yield('content')
+    </body>
+</html>
