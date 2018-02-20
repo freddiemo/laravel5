e7b83bb890f468ed5b8cd009e1f464ab6e72e2ae
freddie molina <freddiejmo@gmail.com>
2018-02-20 18:58:26 -0430

    added the user relationship on the Task model

--- app/Task.php
+++ app/Task.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use App\User;
 use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
@@ -12,4 +13,12 @@ class Task extends Model
      * @var array
      */
     protected $fillable = ['name'];
+
+    /**
+     * Get the user that owns the task.
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
