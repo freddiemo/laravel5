5066846e6f29e25df71340e92688904424fa0bc8
freddie molina <freddiejmo@gmail.com>
2018-02-21 23:29:56 -0430

    adding logout

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
+use Illuminate\Http\Request;
+
 class LoginController extends Controller
 {
     /*
@@ -36,5 +38,15 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+        /**
+     * Logout
+     */
+    public function logout(Request $request)
+    {
+        $this->guard()->logout();
+        $request->session()->invalidate();
+        return redirect('/tasks');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -30,3 +30,5 @@ Route::delete('/task/{task}', 'TaskController@destroy');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::post('/logout', 'Auth\LoginController@logout')->name('logout');
