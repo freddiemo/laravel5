9f3335b64e9cff421d2735877248fefde00790c5
freddie molina <freddiejmo@gmail.com>
2018-02-21 20:10:40 -0430

    deleting the task

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -76,5 +76,9 @@ class TaskController extends Controller
     public function destroy(Request $request, Task $task)
     {
         $this->authorize('destroy', $task);
+
+        $task->delete();
+
+        return redirect('/tasks');
     }
 }
